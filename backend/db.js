bcc926cd91a4463da7cab641ff0f539b70815580
Ashneel <iashneel@gmail.com>
2024-08-29 20:01:29 +0530

    vercel

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,17 @@
 // db.js
-const { Pool } = require('pg');
-const config = require('./dbConfig'); // Adjust the path to your configuration file
+// const { Pool } = require('pg');
+// const config = require('./dbConfig'); // Adjust the path to your configuration file
 
-const pool = new Pool(config);
+// const pool = new Pool(config);
+
+// module.exports = pool;
+
+require('dotenv').config({ path: '.env.local' });
+
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL,
+});
 
 module.exports = pool;
--- backend/dbConfig.js
+++ backend/dbConfig.js
@@ -1,8 +1,18 @@
+// module.exports={
+//   user: "postgres",
+//   host: "localhost",
+//   database: "receipe",
+//   password: "root",
+//   port: 5433,
+// };
+
+
+
 module.exports={
-  user: "postgres",
-  host: "localhost",
-  database: "receipe",
-  password: "root",
-  port: 5433,
+  user: process.env.POSTGRES_URL,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DATABASE,
+  password: process.env.POSTGRES_PASSWORD,
+  port: 5432,
 };
 
